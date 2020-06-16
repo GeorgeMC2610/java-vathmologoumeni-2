7a515e36910b378d34fd25f11be33cf9cd141b88
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-06-16 18:33:07 +0300

    completed the slide 17

---
+++ vathmologoumeni_2/Examples/Slide19/Main.java
@@ -0,0 +1,2 @@
+//Slide 111 of 208, Example 19 of 32
+//Constructors
---
+++ vathmologoumeni_2/Examples/Slide19/Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+    public static final String University = "University of Piraeus";
+
+    private String fullname, department;
+    private int AM, age;
+
+    public Student(String fullname, int age, int yearOfRegistration)
+    {
+        this.fullname           = fullname;
+        this.age                = age;
+        this.yearOfRegistration = yearOfRegistration;
+    }
+
+}
