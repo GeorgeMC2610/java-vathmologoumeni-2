6b889c59e155d460f4052c6f8d3ca41569b7275e
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-06-17 21:37:08 +0300

    slideeeeeeeeeee 23

--- vathmologoumeni_2/Examples/Slide23/Main.java
+++ vathmologoumeni_2/Examples/Slide23/Main.java
@@ -20,6 +20,39 @@ public class Main
         integers.add(a2);
         integers.add(26);
 
+        //and we can output the list to a String like this
         System.out.println(integers.toString());
+
+        //now the size of the list is 3.
+        System.out.println(integers.size());
+
+        //we can remove any object by identifying its index
+        integers.remove(2);
+        System.out.println(integers.toString());
+        System.out.println("Number 26 got removed because the first element is 0.");
+
+        integers.add(29);
+        integers.add(39);
+        integers.add(21);
+
+        //now that we added more elements to the list, there are four ways we can display/access them
+
+        //Method 1. toString
+        System.out.println("1st method 'toString()':");
+        System.out.println(integers.toString() + "\n");
+
+        //Method 2. foreach
+        System.out.println("2nd method 'foreach':");
+        for (Integer i : integers)
+        {
+            System.out.println(i);
+        }
+
+        //Method 3. hard coded for
+        System.out.println("\n3rd method 'hard corded for':");
+        for (int i = 0; i < integers.size(); i++)
+        {
+            System.out.println(integers.get(i));
+        }
     }
 }
