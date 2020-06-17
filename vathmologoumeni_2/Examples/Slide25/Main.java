40c26e6a71b19c5eb57f36873dcc778555354f83
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-06-18 01:28:17 +0300

    made more shit

--- vathmologoumeni_2/Examples/Slide25/Main.java
+++ vathmologoumeni_2/Examples/Slide25/Main.java
@@ -7,16 +7,24 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int a = 5;
-        int b = 18;
-        int c = 39;
+        System.out.println( absoluteValue(-4) );     //the function with the ternary operator returns an int.
+        System.out.println( absoluteValue(-4f));     //the hard-coded function returns a float.
+        System.out.println("These two functions do the same thing, except it's done with a ternary operator in the first one.");
+    }
 
-        int max;
-        max = (a < b)? b : a;
-        max = (b < c)? c : b;
-        max = (a < c)? c : a;
-        
-        System.out.println(max);
+    //these two function do exactly the same thing.
+    //this one does it with a ternary operator
+    public static int absoluteValue(int i)
+    {
+        return (i < 0)? -i : i;     //inside the parenthesis there is the expression. if it is true, then the left side of the ':' gets executed. if it's false, the left side gets executed?
+    }
 
+    //here's the same thing, except it's more obvious for what's happening.
+    public static float absoluteValue(float i)
+    {
+        if (i < 0)
+            return -i;
+        else
+            return i;
     }
 }
