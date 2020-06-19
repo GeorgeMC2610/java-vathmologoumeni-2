a1b145ee576bf6a7be4c5acde16227b1d4a9eb6c
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-06-20 00:52:26 +0300

    i have to complete this tomorrow

---
+++ vathmologoumeni_2/Examples/Slide29/Main.java
@@ -0,0 +1,19 @@
+package vathmologoumeni_2.Examples.Slide29;
+
+//Slide 184 of 208, Example 29 of 32
+//Casting
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        //casting is when we want to actively convert a variable type to another type
+
+        //here's an float
+        float a1 = 15.9f;
+
+        //what if we wanted to
+        int a2 = (int) a1;
+
+    }
+}
