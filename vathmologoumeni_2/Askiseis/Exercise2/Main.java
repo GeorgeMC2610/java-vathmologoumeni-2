28bec0e18120196e583ad4d5a21fe5609acff683
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2020-06-23 18:51:26 +0300

    Seems like this is the final build

--- vathmologoumeni_2/Askiseis/Exercise2/Main.java
+++ vathmologoumeni_2/Askiseis/Exercise2/Main.java
@@ -19,7 +19,9 @@ public class Main
         ints.add(46);
         ints.add(35);
         ints.add(76);
+
         System.out.println("Current state of the list: " + ints.toString());
+        System.out.println("With regular for loops we can just modify elements, e.g. adding 5 to each element of the list.");
 
         //in a loop we can only modify the elements of a list:
         for (int i = 0; i < ints.size(); i++)
@@ -30,17 +32,30 @@ public class Main
 
         System.out.println("Current state of the list: " + ints.toString());
         System.out.println("We can't add or remove something from the list in a loop!");
+        System.out.println("For that purpose we're going to use an iterator.");
 
         //for that purpose there is the list iterator
         ListIterator<Integer> iterator = ints.listIterator();
 
         while (iterator.hasNext())
         {
-            
+            //we can now remove any even element
+            if (iterator.next() % 2 == 0)
+                iterator.remove();
         }
 
         System.out.println("Current state of the list: " + ints.toString());
 
-        
+
+        //and we can do this with reverse traversion
+        while (iterator.hasPrevious())
+        {
+            //we can also now add elements
+            if (iterator.previous() % 2 != 0)
+                iterator.add(10);
+        }
+
+        System.out.println("We can also do this with backwards traversion.");
+        System.out.println("Current state of the list: " + ints.toString());
     }
 }
